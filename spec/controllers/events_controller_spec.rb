90c1a4449a0fc77c636d74fa65db8b5446d1df34
Cristina Iannone <iannonecristina@gmail.com>
2017-06-20 22:57:36 +0100

    Add form for Log Out button and log out test scenario

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe EventsController, type: :controller do
 
   describe "events controller" do
-    it "responds with 302 if user is not signed in" do
+    it "responds with 200" do
       get :index
-      expect(response.status).to eq(302)
+      expect(response.status).to eq(200)
     end
   end
 
--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -9,5 +9,10 @@ RSpec.feature "Sessions", type: :feature do
     expect(current_path).to eq '/'
   end
 
-
+  scenario "User can log out", type: :feature do
+    visit '/users/sign_up'
+    sign_up
+    click_button 'Log out'
+    expect(page).to have_content ('Signed out successfully')
+  end
 end
