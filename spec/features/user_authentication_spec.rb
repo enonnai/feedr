bc3133789ad46d7fe1b7783b62add16e9294f14a
Cristina Iannone <iannonecristina@gmail.com>
2017-06-20 23:03:31 +0100

    Add link for Log In and log in test scenario

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -15,4 +15,12 @@ RSpec.feature "Sessions", type: :feature do
     click_button 'Log out'
     expect(page).to have_content ('Signed out successfully')
   end
+
+  scenario "User can log back in after signing up and logging out", type: :feature do
+    visit '/users/sign_up'
+    sign_up
+    click_button 'Log out'
+    click_link 'Log in'
+    expect(current_path).to eq '/users/sign_in'
+  end
 end
