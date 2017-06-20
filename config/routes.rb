d2719547c66341b99e072709f228f9b54b0e32e1
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-20 16:12:26 +0100

    fixed remaining merge conflicts:

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :events
 
-=======
   root to: 'home#index'
->>>>>>> devise-sign-in
 end
