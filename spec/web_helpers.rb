8970711820b194e46a323aa57245aaf9699d757c
Cristina Iannone <iannonecristina@gmail.com>
2017-06-20 23:40:36 +0100

    Add create_event web helper and feature test for creating events

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -7,3 +7,12 @@ def sign_up(email = "test@gmail.com")
   fill_in 'user_password_confirmation', with: "password"
   click_button 'Sign up'
 end
+
+def create_event
+  visit 'events/new'
+  fill_in "event_title", with: "Mexican dinner"
+  fill_in "event_info", with: "Chilli con carne and margaritas for all. Sombreros and drinks welcome!"
+  fill_in "event_date_time", with: "Friday at 8 PM"
+  fill_in "event_guests", with: 6
+  click_button "Publish"
+end
