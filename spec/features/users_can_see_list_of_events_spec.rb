43356b777973534528e3fcf1f5ebe0a451618831
david gray <davidseangray@gmail.com>
2017-06-21 15:27:06 +0100

    users can see a list of events on the page

---
+++ spec/features/users_can_see_list_of_events_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'web_helpers'
+
+
+RSpec.feature "Events Page", type: :feature do
+
+  scenario "users can see a list of events" do
+    sign_up
+    click_link 'Add your Event'
+    create_event
+    expect(page).to have_content("Chilli con carne and margaritas for all. Sombreros and drinks welcome!")
+  end
+
+end
