c29b6096c901a0871790e1b56814ced6cb3d654c
Cristina Iannone <iannonecristina@gmail.com>
2017-06-21 11:42:01 +0100

    Add feature test for Profiles

--- spec/features/user_profile_have_attributes_spec.rb
+++ spec/features/user_profile_have_attributes_spec.rb
@@ -1,8 +1,11 @@
 require './app/models/user.rb'
 require 'rails_helper'
+require 'web_helpers'
 
 RSpec.feature 'User profiles', type: :feature do
   scenario 'User profiles have name attribute' do
+    visit 'users/sign_up'
+    sign_up
     visit '/profiles/new'
     expect(page).to have_content "Name"
   end
