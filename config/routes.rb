fb4fcf369a59d323811c049a53dfc746fe812c8e
Cristina Iannone <iannonecristina@gmail.com>
2017-06-21 18:09:58 +0100

    Add resources to users and profiles together

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'events#index'
   resources :events
   resources :profiles
+  resources :users do
+    resources :profiles
+  end
 end
