c440cf67ff4df549595400a68f5bf7580ddb871f
Cristina Iannone <iannonecristina@gmail.com>
2017-06-22 16:50:17 +0100

    Refactor user authentication tests

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -12,15 +12,15 @@ RSpec.feature "Sessions", type: :feature do
   scenario "User can log out", type: :feature do
     visit '/users/sign_up'
     sign_up
-    click_button 'Log out'
+    click_link 'Log Out'
     expect(page).to have_content ('Signed out successfully')
   end
 
   scenario "User can log back in after signing up and logging out", type: :feature do
     visit '/users/sign_up'
     sign_up
-    click_button 'Log out'
-    click_link 'Log in'
+    click_link 'Log Out'
+    click_link 'Log In'
     expect(current_path).to eq '/users/sign_in'
   end
 end
