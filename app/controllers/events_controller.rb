81bb59e15bd8ec9b4aeb9f156d3cee56c790cc2b
david gray <davidseangray@gmail.com>
2017-06-22 18:19:17 +0100

    name field appears on sign up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.user_id = current_user.id
+    @event.host_name = user.name
     @event.save
     flash_message
     redirect_to '/events'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621100139) do
+ActiveRecord::Schema.define(version: 20170622165930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170621100139) do
     t.string "info"
     t.string "date_time"
     t.integer "nr_guests"
+    t.string "host_name"
   end
 
   create_table "profiles", force: :cascade do |t|
