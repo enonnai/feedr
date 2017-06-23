666cd46693e734dc327afe39c94692cddbfb425f
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-06-23 11:27:08 +0100

    refactors test for request events

--- spec/features/create_event_spec.rb
+++ spec/features/create_event_spec.rb
@@ -10,4 +10,12 @@ RSpec.feature "Events", type: :feature do
     expect(page).to have_content("Your event has been added")
     expect(page).to have_content("Santa Claus")
   end
+
+  scenario "users can see the event description", type: :feature do
+    sign_up
+    click_link "Add your Event"
+    create_event
+    click_link "View Event"
+    expect(page).to have_content("Chilli con carne and margaritas for all. Sombreros and drinks welcome!")
+  end
 end
--- spec/features/users_can_request_to_attend_an_event_spec.rb
+++ spec/features/users_can_request_to_attend_an_event_spec.rb
@@ -2,20 +2,7 @@ require 'rails_helper'
 require 'web_helpers'
 
 RSpec.feature "Event Requests", type: :feature do
-
   scenario "users can request to attend an event" do
-    visit "users/sign_up"
-    sign_up
-    create_profile
-    click_button "Return to Events"
-    click_link "Add your Event"
-    create_event
-    click_link "View Event"
-    expect(page).to have_content("Chilli con carne and margaritas for all. Sombreros and drinks welcome!")
-  end
-
-
-  xscenario "users can request to attend an event" do
     visit "users/sign_up"
     sign_up
     click_link "Add your Event"
@@ -23,9 +10,7 @@ RSpec.feature "Event Requests", type: :feature do
     click_button "Log out"
     click_link "Log in"
     sign_up_2
-    click_link "Create Profile"
-    create_profile
-    click_button "View Event"
+    click_link "View Event"
     click_button "RSVP"
     expect(page).to have_content("Tester is attending Ben's Party event")
   end
