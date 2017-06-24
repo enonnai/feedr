45f4592da84d064cd2dc4079d3071cb1230bfb44
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-06-24 13:04:38 +0100

    adds parameters to date start and date finish time

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,11 @@ class Event < ApplicationRecord
   acts_as_votable
 
   validates :title, :info, :date_time, :end_date_time, :nr_guests, presence: true
+  validate :end_date_is_after_start_date
 
+  private
+
+  def end_date_is_after_start_date
+    return errors.add(:end_date_time, "End date/time cannot be before the start date/time") if end_date_time < date_time
+  end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -20,8 +20,8 @@ end
 def create_event
   fill_in "event_title", with: "Mexican dinner"
   fill_in "event_info", with: "Chilli con carne and margaritas for all. Sombreros and drinks welcome!"
-  fill_in "event_date_time", with: "Friday at 8 PM"
-  fill_in "event_end_date_time", with: "Fridat at 12 AM"
+  fill_in "event_date_time", with: "30/07/17, 19:00"
+  fill_in "event_end_date_time", with: "30/07/17, 22:00"
   fill_in "event_nr_guests", with: 6
   click_button "Submit"
 end
