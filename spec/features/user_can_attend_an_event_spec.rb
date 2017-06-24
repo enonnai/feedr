cb3f704ba41dd6be5d78b832ec9522855d8c63d5
Cristina Iannone <iannonecristina@gmail.com>
2017-06-25 00:35:51 +0100

    Adjust indendation

--- spec/features/user_can_attend_an_event_spec.rb
+++ spec/features/user_can_attend_an_event_spec.rb
@@ -28,4 +28,16 @@ RSpec.feature 'Attending Events', type: :feature do
     click_link "View Event"
     expect(page).to have_content("FULL HOUSE! Find another event to attend here.")
   end
+
+  scenario "Users who are going to an event can see their name in the attendees' list", js: true do
+    sign_up
+    click_link "Add your Event"
+    create_event_2
+    click_link "Log Out"
+    sign_up_2
+    click_link "View Event"
+    click_button "I'm Going"
+    expect(page).to have_content("Tester 1")
+  end
+
 end
